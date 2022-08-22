b976ec0fd948ee91b4ad10a59645519451b8ae98
Rohan Kolappa <81287555+RohanKolappa@users.noreply.github.com>
2022-08-22 01:27:03 -0700

    Update AppDelegate.swift

--- FeedFree1/AppDelegate.swift
+++ FeedFree1/AppDelegate.swift
@@ -2,8 +2,7 @@
 //  AppDelegate.swift
 //  FeedFree1
 //
-//  Created by Dinesh Kumar on 6/20/19.
-//  Copyright © 2019 ThinkSoft Systems. All rights reserved.
+//  Created by Rohan Kolappa on 6/23/19.
 //
 
 import UIKit
